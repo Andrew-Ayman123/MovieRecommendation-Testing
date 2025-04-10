2b4a7314cf6d903bf3d58c7968c3077ce8e8a8e6
karim elnekheily <karimmichael2002@yahoo.com>
2025-04-10 18:05:56 +0200

    Updated User & Movie classes

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -1,8 +1,44 @@
 package org.example;
-//TODO
-//we need to check the equality of movies objects
 
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+
+//here I used private final instead of record
 public class Movie {
+    private final String title;
+    private final String id;
+    private final List<String> genres;
 
+    // Constructor
+    public Movie(String title, String id, List<String> genres) {
+        this.title = title;
+        this.id = id;
+        this.genres = genres;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Movie movie)) return false;
+        return Objects.equals(getTitle(), movie.getTitle()) && Objects.equals(getId(), movie.getId()) && Objects.equals(new HashSet<>(this.genres), new HashSet<>(movie.genres));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getId(), getGenres());
+    }
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,6 +1,37 @@
 package org.example;
-//TODO
-//we need to check the equality of User objects
-public class User {
-    
+import java.util.List;
+import java.util.Objects;
+import java.util.HashSet;
+
+
+
+// esta3melt record 3ashan teb2a immutable bs momken nesta3mel private Final instead
+// w record betdina el equals method gahza
+public record User(String name,String id,List<String> favouriteMovieIDS) {
+
+/* we can t use equals in favouriteMoviesId by passing object
+because it compares by order
+so hashset mesh byehtam bel order
+ */
+//we use function equals to check if we are already added the user before ,Preventing Duplicate users
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(id, user.id) && Objects.equals(new HashSet<>(this.favouriteMovieIDS), new HashSet<>(user.favouriteMovieIDS));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id, favouriteMovieIDS);
+    }
+    @Override
+    public String toString() {
+        return "User{" + "name='" + name + '\'' +
+                ", id='" + id + '\'' + ", favouriteMovieIds=" + favouriteMovieIDS +
+                '}';
+    }
+
+
 }
